77b6a30966602858189e3e5d0ebe66d1bdf11bd4
diams <diams.rider@gmail.com>
2019-09-14 20:41:05 +0900

    Develop chapter 1 (start up)

--- src/app/src/main/java/diams/mydns/jp/androidtutorial/MainActivity.kt
+++ src/app/src/main/java/diams/mydns/jp/androidtutorial/MainActivity.kt
@@ -1,37 +1,55 @@
 package diams.mydns.jp.androidtutorial
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.widget.TextView
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val handler = Handler();
+    lateinit var sensorManager : SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 500msecごとに加速度センサを値を表示する
-        Timer().schedule(object : TimerTask() {
-            override fun run() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
 
-                val x = 0.0
-                val y = 0.0
-                val z = 0.0
+    override fun onResume() {
+        super.onResume()
 
-                // 文字を表示する
-                handler.post {
-                    val textView = findViewById<TextView>(R.id.main_acceleration_textView)
-                    textView.text =
-                        "x:" + x + "\n" +
-                        "y:" + y + "\n" +
-                        "z:" + z
-                }
+        val accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(sensorEventListener, accel, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(sensorEventListener)
+    }
+
+    val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(sensorEvent: SensorEvent?) {
+            if (sensorEvent!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                val accel_text = String.format(
+                    "x:%f\n" +
+                    "y:%f\n" +
+                    "z:%f",
+                    0.0,    // X方向の加速度センサの値
+                    0.0,    // Y方向の加速度センサの値
+                    0.0)    // Z方向の加速度センサの値
+
+                findViewById<TextView>(R.id.main_acceleration_textView).text = accel_text
             }
-        }, 0, 500)
+        }
+
+        // 今回は使わない
+        override fun onAccuracyChanged(sensor: Sensor?, accuacy: Int) {
+        }
     }
 }
